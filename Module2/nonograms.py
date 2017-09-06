0a4ca6ef07114f4ea0bb12645d7e326f1b809637
Espen Meidell <espen.meidell@gmail.com>
2017-09-06 13:29:03 +0200

    Created variables

--- Module2/nonograms.py
+++ Module2/nonograms.py
@@ -3,11 +3,11 @@ import sys
 
 
 first_line = sys.stdin.readline().split(" ")
-number_of_columns = int(first_line[0].strip())
+number_of_cols = int(first_line[0].strip())
 number_of_rows = int(first_line[1].strip())
 
-row_constraints = []
-column_constraints = []
+row_specs = []
+col_specs = []
 
 row_counter = 0
 for line in sys.stdin:
@@ -15,9 +15,33 @@ for line in sys.stdin:
     int_array = map(int, str_array)
     if row_counter < number_of_rows:
         row_counter += 1
-        row_constraints.append(int_array)
+        row_specs.append(int_array)
     else:
-        column_constraints.append(int_array)
+        col_specs.append(int_array)
 
-print "row constraints:",row_constraints
-print "column constraints:",column_constraints
+row_specs.reverse()
+
+row_default_domain = [x for x in range(number_of_cols)]
+col_default_domain = [x for x in range(number_of_rows)]
+
+def create_variables_from_spec(spec, default_domain):
+    return map( lambda s: {"domain": filter( lambda d: d + s <= len(default_domain)
+                                           , default_domain[:])
+                          ,"length": s}
+              , spec)
+
+
+
+row_variables = map(lambda s: create_variables_from_spec(s, row_default_domain), row_specs)
+col_variables = map(lambda s: create_variables_from_spec(s, col_default_domain), col_specs)
+
+
+print row_variables[0]
+
+
+
+# print "row specs:",row_specs
+# print "col specs:",col_specs
+
+# print row_default_domain
+# print col_default_domain
