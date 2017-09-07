66dd3de6ff6b30121011235c85300ebdb1268c10
eddern <edvardsj@stud.ntnu.no>
2017-09-07 13:48:43 +0200

    added intersection constraints

--- Module2/nonograms.py
+++ Module2/nonograms.py
@@ -29,7 +29,6 @@ def create_variables_from_spec(spec, default_domain, is_row, number):
                           ,"domain": filter( lambda d: d + s <= len(default_domain)
                                            , default_domain[:])
                           ,"length": s
-                          ,"is_row": is_row
                           ,"number": number}
               , spec)
 
@@ -38,29 +37,54 @@ def makefunc(var_names, expression, envir=globals()):
     args = ",".join(var_names)
     return eval("(lambda " + args + ": " + expression + ")", envir)
 
+
+# Checks if the index is occupied given a set of variables
+# Used to check intersections
+def occupied_cell(index, variables):
+    for var in variables:
+        start_pos = var["start"]
+        if start_pos == -1:
+            continue
+        if start_pos <= index and start_pos + var["length"] >= index:
+            return True
+    return False
+
 pair_constraint = makefunc(["a", "b"], "b['start'] > a['start'] + a['length']")
-
-#intersection_constraint = makefunc(["a", "b"], "")
-
-# print apply(pair_constraint, [{"start": 0, "length": 2}, {"start":3, "length": 1}])
+intersection_constraint = makefunc(["a", "b"], "occupied_cell(b[0]['number'], a) == occupied_cell(a[0]['number'], b)")
 
 row_variables = map(lambda (i,s): create_variables_from_spec(s, row_default_domain, True, i), enumerate(row_specs))
 col_variables = map(lambda (i,s): create_variables_from_spec(s, col_default_domain, False, i), enumerate(col_specs))
 
 
 constraints = []
-
+# row pair_constraints
 for i in range(len(row_variables)):
     for j in range(len(row_variables[i])-1):
-        constraints.append((row_variables[i][j], row_variables[i][j+1]))
+        constraints.append(
+            {
+                "variables":(row_variables[i][j], row_variables[i][j+1]),
+                "function": pair_constraint
+            })
 
+# col pair_constraints
 for i in range(len(col_variables)):
     for j in range(len(col_variables[i])-1):
-        constraints.append((col_variables[i][j], col_variables[i][j+1]))
+        constraints.append(
+            {
+                "variables":(col_variables[i][j], col_variables[i][j+1]),
+                "function": pair_constraint
+            })
+
+# all intersection_constraints
+for row in row_variables:
+    for col in col_variables:
+        constraints.append(
+            {
+                "variables":(row,col),
+                "function": intersection_constraint
+            })
 
 
-for row in col_variables:
-    print row
 
 # print "row specs:",row_specs
 # print "col specs:",col_specs
