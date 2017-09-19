9b34f8baeac735ab6efd7c39fe3f50b07ca2e84f
Espen Meidell <espen.meidell@gmail.com>
2017-09-19 12:47:25 +0200

    Refactoring

--- Module2/nonograms_2.py
+++ Module2/nonograms_2.py
@@ -8,6 +8,9 @@ from astar import astar
 import itertools
 import cProfile
 
+'''
+        Read specifications from input
+'''
 
 first_line = sys.stdin.readline().split(" ")
 number_of_cols = int(first_line[0].strip())
@@ -28,6 +31,11 @@ for line in sys.stdin:
 
 row_specs.reverse()
 
+'''
+        Functions to create variables, domains and
+        constraints for the specifications
+'''
+
 # for example: [1,2] results in the following list: [1,0,1,1]
 def generate_minimum_placement(spec):
     insert_positions = [0]
@@ -41,17 +49,10 @@ def generate_minimum_placement(spec):
 
 
 def create_domain(length, specifications):
-    # generate minimum placement
-    domain = []
-    min_placement = []
-    for s in specifications:
-        for i in range(s):
-            min_placement.append(1)
-        min_placement.append(0)
-    min_placement.pop(len(min_placement) - 1)
 
-    insert_indices = [i + 1 for i, x in enumerate(min_placement) if x == 0]
-    insert_indices.extend([0, len(min_placement)])
+    min_placement, insert_indices = generate_minimum_placement(specifications)
+    domain = []
+
     combinations = itertools.combinations_with_replacement(insert_indices, length - len(min_placement))
     for c in combinations:
         result = min_placement[:]
@@ -65,9 +66,7 @@ def create_domain(length, specifications):
     return domain
 
 
-domains = {}
-
-def create_variables_2(specifications, is_row, target_length):
+def create_variables(specifications, is_row, target_length):
     variables = []
     for (i, spec) in enumerate(specifications):
         variable_name = "R" + str(i) if is_row else "C" + str(i)
@@ -75,16 +74,30 @@ def create_variables_2(specifications, is_row, target_length):
         variables.append(variable_name)
     return variables
 
-variables2 = create_variables_2(row_specs, True, number_of_cols)
-variables2.extend(create_variables_2(col_specs, False, number_of_rows))
+'''
+        Define the variables, domains and constraint pairs
+'''
 
-constraint_pairs = list(itertools.product( filter(lambda v: v[0] == 'R', variables2)
-                                         , filter(lambda v: v[0] == 'C', variables2)))
-constraint_pairs.extend(itertools.product( filter(lambda v: v[0] == 'C', variables2)
-                                         , filter(lambda v: v[0] == 'R', variables2)))
+domains = {}
+
+variables = create_variables(row_specs, True, number_of_cols)
+variables.extend(create_variables(col_specs, False, number_of_rows))
+
+constraint_pairs = list(itertools.product( filter(lambda v: v[0] == 'R', variables)
+                                         , filter(lambda v: v[0] == 'C', variables)))
+constraint_pairs.extend(itertools.product( filter(lambda v: v[0] == 'C', variables)
+                                         , filter(lambda v: v[0] == 'R', variables)))
 
 
+'''
+        Utility functions
+'''
 
+def is_row(variable):
+    return variable[0] == "R"
+
+def get_index_from_variable(variable):
+    return int(variable[1:])
 
 
 def print_result(variables, domain):
@@ -100,36 +113,18 @@ def print_result(variables, domain):
         print colored(' ', 'white', attrs=['reverse', 'blink'])
     print colored(' ', 'white', attrs=['reverse', 'blink']) * (number_of_cols * 2 + 3)
 
-
-def is_row(variable):
-    return variable[0] == "R"
-
-def get_index_from_variable(variable):
-    return int(variable[1:])
-
-def check_constraint(row, row_index, col, col_index):
-    return row[col_index] == col[row_index]
-
-def revise_2(X, Y, domains):
-    new_domain = []
-    for dX in domains[X]:
-        for dY in domains[Y]:
-            if is_row(X):
-                if check_constraint(dX, get_index_from_variable(X), dY, get_index_from_variable(Y)):
-                    if dX not in new_domain:
-                        new_domain.append(dX)
-                    continue
-            else:
-                if check_constraint(dY, get_index_from_variable(Y), dX, get_index_from_variable(X)):
-                    if dX not in new_domain:
-                        new_domain.append(dX)
-                    continue
-    reduced = len(domains[X]) > len(new_domain)
-    domains[X] = new_domain
-    return reduced
+'''
+        A* Functions
+'''
 
 def find_successor(open_set, cost, heuristic):
-    return open_set[0]
+    bestcost = float("inf")
+    bestboard = None
+    for board in open_set:
+        if heuristic(board) < bestcost:
+            bestboard = board
+            bestcost = heuristic(board)
+    return bestboard
 
 # Create successor state by creating new triples where the domain is cloned and reduced
 def generate_successors(current):
@@ -149,8 +144,9 @@ def generate_successors(current):
                 successors.append((current[0], child_domain, current[2]))
     return successors
 
+# The sum of the length of the domains in the current state
 def heuristic(state):
-    return 0
+    return sum(map(lambda d: len(d), state[1].values()))
 
 # Check if the domain length for each variable is 1
 def is_terminal(state):
@@ -160,16 +156,41 @@ def is_terminal(state):
 def hash_function(s):
     return str(s[1])
 
+'''
+        CSP Functions
+'''
+
+def check_constraint(row, row_index, col, col_index):
+    return row[col_index] == col[row_index]
+
+def revise(X, Y, domains):
+    new_domain = []
+    for dX in domains[X]:
+        for dY in domains[Y]:
+            if is_row(X):
+                if check_constraint(dX, get_index_from_variable(X), dY, get_index_from_variable(Y)):
+                    if dX not in new_domain:
+                        new_domain.append(dX)
+                    continue
+            else:
+                if check_constraint(dY, get_index_from_variable(Y), dX, get_index_from_variable(X)):
+                    if dX not in new_domain:
+                        new_domain.append(dX)
+                    continue
+    reduced = len(domains[X]) > len(new_domain)
+    domains[X] = new_domain
+    return reduced
+
 def domain_filtering_loop(queue, domains, constraints):
     while queue:
         X, Y = queue.popleft()
-        reduced = revise_2(X, Y, domains)
+        reduced = revise(X, Y, domains)
         if reduced:
             for Ck in constraints:
                 if X == Ck[1]:
                     queue.append(Ck)
 
-def solve_2(variables, domains, constraints):
+def solve(variables, domains, constraints):
     queue = deque([])
     for c in constraints:
         queue.append(c)
@@ -178,18 +199,15 @@ def solve_2(variables, domains, constraints):
 
     if len(filter(lambda v: len(domains[v]) > 1, variables)) != 0:    # are all domains reduced to 1
         result = astar((variables, domains, constraints)
-                    , find_successor
-                    , generate_successors
-                    , heuristic
-                    , is_terminal
-                    , hash_function)
+                      , find_successor
+                      , generate_successors
+                      , heuristic
+                      , is_terminal
+                      , hash_function)
         print_result(result[1][0], result[1][1])
     else:
         print_result(variables, domains)
 
 
 
-# cProfile.run('solve(variables, constraints)')
-solve_2(variables2, domains, constraint_pairs)
-# for variable in row_variables:
-#     print len(variable["domain"])
+cProfile.run('solve(variables, domains, constraint_pairs)')
